9864660ca2f600f38302bac68928c7326d553bcd
Somesh <somesh_nj2_076@masai.school>
2021-10-09 13:24:05 +0530

    listing page

--- src/components/BookPage/BookMain.jsx
+++ src/components/BookPage/BookMain.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from "../../styles/LandingPage.module.css";
+import { convertDates } from '../../utils/convertDates';
 const book = {
         "_id": "616118fa212087150436ea4d",
         "title": "Intelligent Investor",
@@ -17,14 +18,22 @@ const book = {
 }
 
 export const BookMain = () => {
+     const date = convertDates(book.createdAt);
 return <div className={styles.container_list}>
         <div className={styles.book_list_1}>
-        <span>{book.title}</span>
+        <span><strong>{book.title}</strong> posted on { date}</span>
         <img src={ book.featureImg} alt=""/>
         </div>
         <div className={styles.book_list_2}>
-        <span>{book.type}</span>
-        <span>{book.price}</span>
+        <div>Type - {book.type}</div>
+        <div>Price - {book.price}</div>
+        </div>
+    <div className={styles.book_list_3}>
+        <h3>About Book</h3>
+        <div>{book.author}</div>
+        <div>{book.condition}</div>
+        <div>{book.location}</div>
+        <div>{book.description}</div>
         </div>
     </div>
 }
