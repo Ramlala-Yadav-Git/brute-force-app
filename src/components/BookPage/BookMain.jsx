bcf98e9f7c29d738cc9948c305092a12bfee64f7
= <=>
2021-10-09 15:31:35 +0530

    CreateBook Page And BookMain Page

--- src/components/BookPage/BookMain.jsx
+++ src/components/BookPage/BookMain.jsx
@@ -1,39 +1,99 @@
-import React from 'react';
-import styles from "../../styles/LandingPage.module.css";
-import { convertDates } from '../../utils/convertDates';
+import React from "react";
+import styles from "./BookMain.module.css";
+import { convertDates } from "../../utils/convertDates";
 const book = {
-        "_id": "616118fa212087150436ea4d",
-        "title": "Intelligent Investor",
-        "type": "Rent",
-        "description": "good condition",
-        "featureImg": "http://res.cloudinary.com/someshcloudinary/image/upload/v1633753335/ozcucn74hytplatvbsef.jpg",
-        "price": 10,
-        "author": "Linux Torvalds",
-        "seller": "616115f65cce7f29487d4afb",
-        "condition": "Good",
-        "location": "Valsad",
-        "comments": [],
-        "createdAt": "2021-10-09T04:22:18.042Z",
-        "updatedAt": "2021-10-09T04:22:18.042Z"
-}
+  _id: "616118fa212087150436ea4d",
+  title: "Intelligent Investor",
+  type: "Rent",
+  description:
+    "good conditiongood conditiongood conditiongood conditiongood conditiongood conditiongood conditiongood conditiongood conditiongood condition",
+  featureImg:
+    "http://res.cloudinary.com/someshcloudinary/image/upload/v1633753335/ozcucn74hytplatvbsef.jpg",
+  price: 10,
+  author: "Linux Torvalds",
+  seller: {
+    name: "man",
+    image:
+      "http://res.cloudinary.com/someshcloudinary/image/upload/v1633753335/ozcucn74hytplatvbsef.jpg",
+    email: "Email",
+  },
+  condition: "Good",
+  location: "Valsad",
+  comments: [],
+  createdAt: "2021-10-09T04:22:18.042Z",
+  updatedAt: "2021-10-09T04:22:18.042Z",
+};
 
 export const BookMain = () => {
-     const date = convertDates(book.createdAt);
-return <div className={styles.container_list}>
-        <div className={styles.book_list_1}>
-        <span><strong>{book.title}</strong> posted on { date}</span>
-        <img src={ book.featureImg} alt=""/>
+  const date = convertDates(book.createdAt);
+  return (
+    <div className={styles.maindiv}>
+      <div className={styles.div}>
+        <div className={styles.imgdiv}>
+          <img className={styles.image} src={book.featureImg} alt="" />
         </div>
-        <div className={styles.book_list_2}>
-        <div>Type - {book.type}</div>
-        <div>Price - {book.price}</div>
+        <div>
+          <h1>{book.title}</h1>
+          <h5
+            style={{ color: "gray", marginTop: "10px", marginBottom: "10px" }}
+          >
+            By:-{book.author}
+          </h5>
+          <div>
+            <p className={styles.p}>{book.description}</p>
+            <h5
+              style={{ color: "gray", marginTop: "10px", marginBottom: "10px" }}
+            >
+              Date:-{date}
+            </h5>
+            <h5
+              style={{ color: "gray", marginTop: "10px", marginBottom: "10px" }}
+            >
+              Location:-{book.location}
+            </h5>
+          </div>
+          <div className={styles.last}>
+            <h4>Condition:-{book.condition}</h4>
+            <h4>{book.type}</h4>
+            <h4>Price:-{book.price}</h4>
+            <button>Buy Now</button>
+          </div>
         </div>
-    <div className={styles.book_list_3}>
-        <h3>About Book</h3>
-        <div>{book.author}</div>
-        <div>{book.condition}</div>
-        <div>{book.location}</div>
-        <div>{book.description}</div>
+      </div>
+
+      <div className={styles.description}>
+        <h3>Seller's Info</h3>
+        <div className={styles.seller}>
+          <img src={book.seller.image} alt="" />
+          <div>
+            <h4>{book.seller.name}</h4>
+            <p>{book.seller.email}</p>
+          </div>
         </div>
+      </div>
+      <div className={styles.description}>
+        <h3>Book Description</h3>
+        <p>{book.description}</p>
+      </div>
     </div>
-}
+    // <div className={styles.container_list}>
+    //   <div className={styles.book_list_1}>
+    //     <span>
+    //       <strong>{book.title}</strong> posted on {date}
+    //     </span>
+    //     <img src={book.featureImg} alt="" />
+    //   </div>
+    //   <div className={styles.book_list_2}>
+    //     <div>Type - {book.type}</div>
+    //     <div>Price - {book.price}</div>
+    //   </div>
+    //   <div className={styles.book_list_3}>
+    //     <h3>About Book</h3>
+    //     <div>{book.author}</div>
+    //     <div>{book.condition}</div>
+    //     <div>{book.location}</div>
+    //     <div>{book.description}</div>
+    //   </div>
+    // </div>
+  );
+};
---
+++ src/components/CreateBook/CreateBook.jsx
@@ -0,0 +1,210 @@
+import React, { useState } from "react";
+import styles from "./CreateBook.module.css";
+import Pincode from "react-pincode";
+import axios from "axios";
+
+const CreateBook = () => {
+  const [pinlocation, setpinlocation] = useState({ find: false });
+
+  const getDetails = (pin) => {
+    let location = {};
+    axios
+      .get(`https://api.postalpincode.in/pincode/${pin} `)
+      .then((res) => {
+        location = {
+          find: true,
+          city: res.data[0].PostOffice[0].Block,
+          district: res.data[0].PostOffice[0].District,
+          state: res.data[0].PostOffice[0].State,
+        };
+        setpinlocation(location);
+      })
+      .catch(function (error) {
+        alert("Wrong Pincode");
+      });
+  };
+
+  const [showPrice, setShowPrice] = useState(false);
+  const handleChange = (e) => {
+    if (e.target.name === "pincode") {
+      if (e.target.value.length === 6) {
+        getDetails(e.target.value);
+        console.log("pin", pinlocation);
+      } else {
+        setpinlocation({});
+      }
+    }
+    if (
+      e.target.name === "type" &&
+      (e.target.value === "Rent" || e.target.value === "Sell")
+    ) {
+      setShowPrice(true);
+    } else if (e.target.name === "type" && e.target.value === "Donate") {
+      setShowPrice(false);
+    }
+  };
+  return (
+    <div className={styles.body}>
+      <div className={styles.mainDiv}>
+        <h2 style={{ textAlign: "center" }}>SELL/DONATE A BOOK</h2>
+        <form action="">
+          <div>
+            <label htmlFor="title">Book Title</label>
+            <input
+              onChange={handleChange}
+              type="text"
+              name="title"
+              id="title"
+              placeholder="Enter Title Of The Book"
+            />
+          </div>
+          <div>
+            <label htmlFor="author">Author Name</label>
+            <input
+              onChange={handleChange}
+              type="text"
+              name="author"
+              id="author"
+              placeholder="Enter Author Name Of The Book"
+            />
+          </div>
+          <div className={styles.two}>
+            <div>
+              <div>
+                <p>Choose One</p>
+                <div className={styles.type}>
+                  <input
+                    onChange={handleChange}
+                    type="radio"
+                    name="type"
+                    id="type1"
+                    value="Rent"
+                  />
+                  <label htmlFor="type1">Rent</label>
+                  <input
+                    onChange={handleChange}
+                    type="radio"
+                    name="type"
+                    id="type2"
+                    value="Sell"
+                  />
+                  <label htmlFor="type2">Sell</label>
+                  <input
+                    onChange={handleChange}
+                    type="radio"
+                    name="type"
+                    id="type3"
+                    value="Donate"
+                  />
+                  <label htmlFor="type3">Donate</label>
+                </div>
+              </div>
+              {showPrice ? (
+                <div>
+                  <label htmlFor="price">Enter Price</label>
+                  <input
+                    type="number"
+                    name="price"
+                    id="price"
+                    placeholder="Enter Price Of The Book"
+                  />
+                </div>
+              ) : null}
+            </div>
+            <div>
+              <p>Condition of Book</p>
+              <div className={styles.type}>
+                <input
+                  onChange={handleChange}
+                  type="radio"
+                  name="condition"
+                  id="typec1"
+                  value="Readable"
+                />
+                <label htmlFor="typec1">Readable</label>
+                <input
+                  onChange={handleChange}
+                  type="radio"
+                  name="condition"
+                  id="typec2"
+                  value="Good"
+                />
+                <label htmlFor="typec2">Good</label>
+                <input
+                  onChange={handleChange}
+                  type="radio"
+                  name="condition"
+                  id="typec3"
+                  value="Best"
+                />
+                <label htmlFor="typec3">Best</label>
+              </div>
+            </div>
+            <div>
+              <p>Choose Image</p>
+              <div>
+                <input type="file" name="image" id="" />
+              </div>
+            </div>
+          </div>
+          <div>
+            <label htmlFor="location">Pincode</label>
+            <input
+              onChange={handleChange}
+              type="number"
+              name="pincode"
+              id="location"
+              placeholder="Enter Pincode"
+            />
+          </div>
+          {pinlocation.find ? (
+            <div className={styles.location}>
+              <div>
+                <label htmlFor="state">State</label>
+                <input
+                  value={pinlocation.state}
+                  onChange={handleChange}
+                  type="text"
+                  name="state"
+                  id="state"
+                  placeholder="Enter state"
+                />
+              </div>
+              <div>
+                <label htmlFor="district">District</label>
+                <input
+                  value={pinlocation.district}
+                  onChange={handleChange}
+                  type="text"
+                  name="district"
+                  id="district"
+                  placeholder="Enter district"
+                />
+              </div>
+              <div>
+                <label htmlFor="city">City</label>
+                <input
+                  value={pinlocation.city}
+                  onChange={handleChange}
+                  type="text"
+                  name="city"
+                  id="city"
+                  placeholder="Enter city"
+                />
+              </div>
+            </div>
+          ) : null}
+          <div>
+            <label htmlFor="author">Description</label>
+            <textarea name="description" id="" cols="30" rows="5"></textarea>
+          </div>
+          <div className={styles.submit}>
+            <input type="submit" value="CREATE" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CreateBook;
