a5a6071418e3b293a502954cecd4fa3b9b4b823c
Somesh <somesh_nj2_076@masai.school>
2021-10-10 02:26:46 +0530

    chat box

--- src/components/ListingPage/ChatBox.jsx
+++ src/components/ListingPage/ChatBox.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "../../styles/LandingPage.module.css";
 import styled from "styled-components";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 const Tabs = styled.span`
 text-transform: uppercase;
@@ -13,11 +15,32 @@ border-bottom:${props => (props.tab === props.val) ? "1px solid #292929" : "none
 `
 
 export const ChatBox = () => {
+     const [chat, setChat] = React.useState([]);
+    const user = useSelector((state) => state.auth.user.user); 
     const [text, setText] = React.useState("");
     const handleClick = () => {
+        if (text.length === 0) {
+            return
+        }
+        axios.post(`http://localhost:2345/global`, { text:text,author:user._id });
+        axios.get(`http://localhost:2345/global`).then(res => {
+                setChat(res.data.data);
+            }); 
         setText("");
     }
+    React.useEffect(() => {
+        axios.get(`http://localhost:2345/global`).then(res => {
+            setChat(res.data.data);
+        });
+        setInterval(() => {
+            axios.get(`http://localhost:2345/global`).then(res => {
+                setChat(res.data.data);
+            });
+        }, 5000)
+    },[]);
     const [tabs, setTabs] = React.useState(true);
+    console.log(chat)
+
     return <div className={styles.chat_box}>
         <div>
             <div className={styles.chat_box_2}> <Tabs onClick={() => setTabs(false)} tab={tabs} val={false}>
@@ -27,6 +50,14 @@ export const ChatBox = () => {
           Local
         </Tabs></div>
             <div className={styles.chat_box_3}>
+                {!tabs && chat.map(item => {
+                        return <div>
+                            {user._id === item.author._id ? <div className={styles.right_chat}>{item.text}</div> : <div className={styles.left_chat}>
+                                <div><strong>{item.author.name}</strong></div>
+                                <div>{item.text}</div>
+                            </div>}
+                        </div>
+                    })}
             </div>
             <div onClick={handleClick} className={styles.chat_box_4}><input value={text} onChange={(e) => { setText(e.target.value)}}/><button>Send</button></div>
         </div>
