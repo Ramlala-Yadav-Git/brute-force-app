7942228039ad3bc2b3c9817adf319951071863cc
Somesh <somesh_nj2_076@masai.school>
2021-10-10 01:06:24 +0530

    merging

--- src/components/BookPage/Comments.jsx
+++ src/components/BookPage/Comments.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 import styles from "../../styles/LandingPage.module.css";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 export const Comments = () => {
-const [text, setText] = React.useState("");
+    const [chat, setChat] = React.useState([]);
+    const { id } = useParams();
+    const user = useSelector((state) => state.auth.user.user); 
+    const [text, setText] = React.useState("");
+    React.useEffect(() => {
+        axios.post(`http://localhost:2345/${user._id}/book/${id}`).then(res => {
+            setChat(res.data.data);
+        });
+        setInterval(() => {
+            axios.post(`http://localhost:2345/${user._id}/book/${id}`).then(res => {
+                setChat(res.data.data);
+            });  
+        },5000)
+    },[id,user._id])
+
+    console.log(chat);
     const handleClick = () => {
+        if (text.length === 0) {
+            return
+        }
+        axios.patch(`http://localhost:2345/${user._id}/book/${id}`, { text });
+        axios.post(`http://localhost:2345/${user._id}/book/${id}`).then(res => {
+                setChat(res.data.data);
+            }); 
         setText("");
     }
     const [tabs, setTabs] = React.useState(true);
@@ -14,6 +39,11 @@ const [text, setText] = React.useState("");
         </span>
         </div>
             <div className={styles.chat_box_3}>
+                {
+                    chat.map(item => {
+                        return <div>{item.text}</div>
+                    })
+                }
             </div>
             <div onClick={handleClick} className={styles.chat_box_4}><input value={text} onChange={(e) => { setText(e.target.value)}}/><button>Send</button></div>
         </div>
