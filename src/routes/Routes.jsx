85cd2ec0b2f2c85f42b43f07ec8106341ebe3cfc
Ramlala Yadav <easyexam01@gmail.com>
2021-10-09 19:04:34 +0530

    added routing

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Switch,Route} from "react-router-dom"
+import { Switch, Route } from "react-router-dom"
 import { BookPage } from "../components/BookPage/BookPage";
 import { LandingPage } from "../components/LandingPage/LandingPage";
 import { ListingPage } from "../components/ListingPage/ListingPage";
